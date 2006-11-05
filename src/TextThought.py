1a22a440762aca391fef07e2e16c586e9e3225ef
DonScorgie <DonScorgie@8f060a39-251c-0410-b1f3-431655927647>
2006-11-05 15:22:50 +0000

    * src/TextThought.py: Make backspace work again at end of text
    git-svn-id: https://labyrinth.googlecode.com/svn/trunk@112 8f060a39-251c-0410-b1f3-431655927647

--- src/TextThought.py
+++ src/TextThought.py
@@ -363,7 +363,7 @@ class TextThought (BaseThought.BaseThought):
 			bleft = self.bytes[:self.b_f_i(self.index)-1]
 			bright = self.bytes[self.b_f_i(self.index):]
 			local_text = self.text[self.index-int(self.bytes[self.bindex-1]):self.index]
-			local_bytes = self.bytes[self.b_f_i(self.index-1)]
+			local_bytes = self.bytes[self.b_f_i(self.index)-1]
 			self.index-=int(self.bytes[self.bindex-1])
 		self.text = left+right
 		self.bytes = bleft+bright
