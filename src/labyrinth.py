d932e2a5eb15a44b9b7366fe14e68e8dacdc7c80
matthias.vogelgesang <matthias.vogelgesang@8f060a39-251c-0410-b1f3-431655927647>
2008-04-19 17:08:03 +0000

    * src/labyrinth.py Minor cleanups like indentation.
    git-svn-id: https://labyrinth.googlecode.com/svn/trunk@324 8f060a39-251c-0410-b1f3-431655927647

--- src/labyrinth.py
+++ src/labyrinth.py
@@ -3,6 +3,7 @@
 # This file is part of Labyrith
 #
 # Copyright (C) 2006 - Don Scorgie <Don@Scorgie.org>
+# Copyright (C) 2008 - Labyrinth-Dev-Team
 #
 # Labyrith is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,23 +22,21 @@
 #
 
 import pygtk
-
 import gettext, locale
 import optparse
-import sys
-from os.path import *
-import os
+import sys, os
+import os.path as osp
 
 if os.name != 'nt':
 	pygtk.require('2.0')
 	def _check (path):
-		return exists(path) and isdir(path) and isfile(path+"/AUTHORS")
+		return osp.exists(path) and osp.isdir(path) and osp.isfile(path+"/AUTHORS")
 
-	name = join(dirname(__file__), '..')
+	name = osp.join(osp.dirname(__file__), '..')
 	if _check(name):
-			sys.path.insert(0, abspath(name))
+		sys.path.insert(0, osp.abspath(name))
 	else:
-			sys.path.insert(0, abspath("@PYTHONDIR@"))
+		sys.path.insert(0, osp.abspath("@PYTHONDIR@"))
 
 import gtk
 
@@ -51,11 +50,12 @@ try:
 except:
 	localedir = ""
 
-
 gettext.bindtextdomain('labyrinth', localedir)
 if hasattr(gettext, 'bind_textdomain_codeset'):
 	gettext.bind_textdomain_codeset('labyrinth','UTF-8')
+
 gettext.textdomain('labyrinth')
+
 if not os.name == 'nt':
 	locale.bindtextdomain('labyrinth', localedir)
 	if hasattr(locale, 'bind_textdomain_codeset'):
@@ -65,33 +65,30 @@ if not os.name == 'nt':
 gtk.glade.bindtextdomain('labyrinth')
 gtk.glade.textdomain('labyrinth')
 
-
 def main():
 	parser = optparse.OptionParser()
-        parser.add_option("--use-tray-icon",
-            dest="tray_icon", action="store_true", default=False)
-        parser.add_option("--no-tray-icon",
-            dest="tray_icon", action="store_false" )
-        parser.add_option("--hide-main-window",
-            action="store_true", default=False)
-	parser.add_option("-m", "--map",
-                  action="store", type="string", dest="filename",
-                  help="Open a map from a given filename (from internal database)")
-	parser.add_option("-o", "--open",
-				  action="store", type="string", dest="filepath",
-				  help="Open a map from a given filename (including path)")
+	parser.add_option("--use-tray-icon", dest="tray_icon", 
+		action="store_true", default=False)
+	parser.add_option("--no-tray-icon", dest="tray_icon", action="store_false")
+	parser.add_option("--hide-main-window", action="store_true", default=False)
+	parser.add_option("-m", "--map", action="store", type="string", dest="filename",
+		help="Open a map from a given filename (from internal database)")
+	parser.add_option("-o", "--open", action="store", type="string", 
+		dest="filepath", help="Open a map from a given filename (including path)")
+		
 	(options, args) = parser.parse_args()
-        if not options.tray_icon:
-            options.hide_main_window=False
+	if not options.tray_icon:
+		options.hide_main_window=False
+	
 	MapBrowser = Browser.Browser (
-          start_hidden = options.hide_main_window,
-          tray_icon= options.tray_icon
-        )
+		start_hidden = options.hide_main_window,
+		tray_icon = options.tray_icon
+    )
+	
 	if options.filename != None:
 		MapBrowser.open_map_filename (utils.get_save_dir() + options.filename)
 	elif options.filepath != None:
 		MapBrowser.open_map_filename (options.filepath)
-
 
 	try:
 		gtk.main()
@@ -100,4 +97,4 @@ def main():
 		sys.exit(1)
 
 if __name__ == '__main__':
-		main()
+	main()
