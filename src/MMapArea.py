587874bee824a2ba55ab226a53c4ab0be8681b07
mascha@ma-scha.de <mascha@ma-scha.de@8f060a39-251c-0410-b1f3-431655927647>
2009-02-26 22:00:49 +0000

    o Fix issue 135. Thanks to wks1985 for the patch.
    git-svn-id: https://labyrinth.googlecode.com/svn/trunk@346 8f060a39-251c-0410-b1f3-431655927647

--- src/MMapArea.py
+++ src/MMapArea.py
@@ -797,7 +797,7 @@ class MMapArea (gtk.DrawingArea):
 		height = area.height / self.scale_fac
 		for t in self.thoughts:
 			try:
-				if t.lr[0] >= ax and t.ul[0] <= ax + width and t.lr[1] <= ay + height and t.ul[1] >= ay:
+				if max(t.ul[0],ax)<=min(t.lr[0],ax+width) and max(t.ul[1],ay)<=min(t.lr[1],ay+height0):
 					t.draw (context)
 			except:
 				t.draw(context)
