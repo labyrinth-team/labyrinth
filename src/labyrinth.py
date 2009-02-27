144ad6e36036ae535fd4c9b4be650938ced80537
matthias.vogelgesang <matthias.vogelgesang@8f060a39-251c-0410-b1f3-431655927647>
2009-02-27 17:46:34 +0000

    * src/labyrinth.py Fix issue 133. Thanks to Romain Tartiere.
    git-svn-id: https://labyrinth.googlecode.com/svn/trunk@347 8f060a39-251c-0410-b1f3-431655927647

--- src/labyrinth.py
+++ src/labyrinth.py
@@ -56,11 +56,13 @@ if hasattr(gettext, 'bind_textdomain_codeset'):
 
 gettext.textdomain('labyrinth')
 
-if not os.name == 'nt':
-	locale.bindtextdomain('labyrinth', localedir)
-	if hasattr(locale, 'bind_textdomain_codeset'):
-		locale.bind_textdomain_codeset('labyrinth','UTF-8')
-	locale.textdomain('labyrinth')
+if hasattr(locale, 'bindtextdomain'):
+	if not os.name == 'nt':
+		locale.bindtextdomain('labyrinth', localedir)
+		if hasattr(locale, 'bind_textdomain_codeset'):
+			locale.bind_textdomain_codeset('labyrinth','UTF-8')
+		locale.textdomain('labyrinth')
+
 
 gtk.glade.bindtextdomain('labyrinth')
 gtk.glade.textdomain('labyrinth')
