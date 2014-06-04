19cba2accfdb735f70542004906c644e6fdc8a57
Thomas Kluyver <takowl@gmail.com>
2014-06-04 11:44:20 -0700

    Move launch code inside package

--- labyrinth_lib/launch.py
+++ labyrinth_lib/launch.py
@@ -1,41 +1,66 @@
 import pygtk
 import gettext, locale
+import optparse
 import os
 import os.path as osp
+import sys
 
 pygtk.require('2.0')
 
 import gtk
 
 from labyrinth_lib import Browser
-try:
-    from labyrinth_lib import defs
-    localedir = osp.abspath(osp.join(defs.DATA_DIR, "locale"))
-except:
-    localedir = "/usr/share/locale"
+from labyrinth_lib import utils
 
-gettext.bindtextdomain('labyrinth', localedir)
-if hasattr(gettext, 'bind_textdomain_codeset'):
-    gettext.bind_textdomain_codeset('labyrinth','UTF-8')
+def prepare_locale():
+    if os.name == 'nt':
+        instdir = osp.abspath(osp.dirname(sys.argv[0]))
+        localedir = osp.join(instdir, "locale")
+    else:
+        localedir = "/usr/share/locale"
 
-gettext.textdomain('labyrinth')
+    gettext.bindtextdomain('labyrinth', localedir)
+    if hasattr(gettext, 'bind_textdomain_codeset'):
+        gettext.bind_textdomain_codeset('labyrinth','UTF-8')
 
-if hasattr(locale, 'bindtextdomain'):
-    if not os.name == 'nt':
-        locale.bindtextdomain('labyrinth', localedir)
-        if hasattr(locale, 'bind_textdomain_codeset'):
-            locale.bind_textdomain_codeset('labyrinth','UTF-8')
-        locale.textdomain('labyrinth')
+    gettext.textdomain('labyrinth')
 
+    if hasattr(locale, 'bindtextdomain'):
+        if not os.name == 'nt':
+            locale.bindtextdomain('labyrinth', localedir)
+            if hasattr(locale, 'bind_textdomain_codeset'):
+                locale.bind_textdomain_codeset('labyrinth','UTF-8')
+            locale.textdomain('labyrinth')
 
-gtk.glade.bindtextdomain('labyrinth')
-gtk.glade.textdomain('labyrinth')
+    gtk.glade.bindtextdomain('labyrinth')
+    gtk.glade.textdomain('labyrinth')
 
 
 def main():
+    parser = optparse.OptionParser()
+    parser.add_option("--use-tray-icon", dest="tray_icon",
+            action="store_true", default=False)
+    parser.add_option("--no-tray-icon", dest="tray_icon", action="store_false")
+    parser.add_option("--hide-main-window", action="store_true", default=False)
+    parser.add_option("-m", "--map", action="store", type="string", dest="filename",
+            help="Open a map from a given filename (from internal database)")
+    parser.add_option("-o", "--open", action="store", type="string",
+            dest="filepath", help="Open a map from a given filename (including path)")
+
+    (options, args) = parser.parse_args()
+    if not options.tray_icon:
+        options.hide_main_window=False
+
+    prepare_locale()
+
     MapBrowser = Browser.Browser (
-            start_hidden = False,
-            tray_icon = False
+            start_hidden = options.hide_main_window,
+            tray_icon = options.tray_icon
     )
+
+    if options.filename != None:
+        MapBrowser.open_map_filename (os.path.join (utils.get_save_dir(), options.filename))
+    elif options.filepath != None:
+        MapBrowser.open_map_filename (options.filepath)
 
     gtk.main()
