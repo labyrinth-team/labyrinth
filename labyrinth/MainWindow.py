12d0f5b24cac6a9e87288129fdb29da3eccb713a
Thomas Kluyver <thomas@kluyver.me.uk>
2021-04-03 20:17:06 +0100

    Remove unused method

--- labyrinth/MainWindow.py
+++ labyrinth/MainWindow.py
@@ -442,10 +442,6 @@ class LabyrinthWindow (GObject.GObject):
         self.MainArea.translation = [0.0, 0.0]
         self.MainArea.invalidate()
 
-    def new_window_cb (self, arg):
-        global_new_window ()
-        return
-
     def switch_buffer_cb (self, arg, new_buffer):
         if new_buffer:
             self.extended.set_editable (True)
