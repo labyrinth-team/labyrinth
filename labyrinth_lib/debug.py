0c5bd5e712e43d558a5ffc17bc582c89c6e2a848
Thomas Kluyver <thomas@kluyver.me.uk>
2021-04-22 21:13:16 +0100

    Fix syntax of debugging function

--- labyrinth_lib/debug.py
+++ labyrinth_lib/debug.py
@@ -1,15 +1,18 @@
 """Debugging functions"""
 
+from gi.repository import PangoAttrCast
+
 def show_attrlist(al):
     """Display a pango attribute list."""
     it = al.get_iterator()
     def show_range():
-        print "%d-%d:" % it.range(),
+        print("%d-%d:" % it.range(), end=' ')
         for x in it.get_attrs():
-            print (x.type, x.start_index, x.end_index),
-            if hasattr(x, 'value'):
-                print x.value,
-        print
+            print(x.klass.type, x.start_index, x.end_index, end=' ')
+            int_attr = PangoAttrCast.as_int(x)
+            if int_attr is not None:
+                print(int_attr.value, end=', ')
+        print()
     
     show_range()
     while it.next():
